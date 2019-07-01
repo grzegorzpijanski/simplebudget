0706d73e19725fe59aba3f2497a91bb889fbd3db
Grzegorz Pijański <grzegorz.pijanski@gmail.com>
2019-07-01 12:55:30 +0200

    First commit

---
+++ src/main/java/com/typicaldev/simplebudget/SimplebudgetApplication.java
@@ -0,0 +1,13 @@
+package com.typicaldev.simplebudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplebudgetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplebudgetApplication.class, args);
+    }
+
+}
